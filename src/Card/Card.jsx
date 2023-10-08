4af7b47ecf997c654e8533f84323adebd4b67ff6
trananh-it-hust <haianhtranmanh@gmail.com>
2023-10-08 17:45:33 +0700

    ha final commit

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./index.css";
 const Card = ({ data, handleChange }) => {
   const { done, name, id } = data;
+  const doneClass = (done) => {
+    return done ? "done text" : "text";
+  };
   return (
     <div className="item">
       <input type="checkbox" checked={done} onChange={() => handleChange(id)} />
-      <div className="text">{name}</div>
+      <div className={doneClass(done)}>{name}</div>
     </div>
   );
 };
